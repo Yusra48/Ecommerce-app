3aa944c31bd9088f8ef21310f2d4e92df0317eee
Yusra48 <154058242+Yusra48@users.noreply.github.com>
2024-04-03 15:46:20 +0500

    Update HomeScreen.dart

--- lib/Navigations/HomeScreen.dart
+++ lib/Navigations/HomeScreen.dart
@@ -1,121 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Navigations/Cartview.dart';
 
-class ProductItem {
-  final String imageUrl;
-  final String title;
-  final String description;
-  final String price;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  ProductItem({
-    required this.imageUrl,
-    required this.title,
-    required this.description,
-    required this.price,
-  });
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
-
-  final List<ProductItem> _buildProductItems = [
-    ProductItem(
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2017/08/17/08/20/online-shopping-2650383_960_720.jpg',
-      title: 'Hand Bag ',
-      description: 'Black Tote Bag IDB-AW23-99',
-      price: '\$45',
-    ),
-    ProductItem(
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2017/03/14/11/41/perfume-2142824_960_720.jpg',
-      title: 'Perfume',
-      description:
-          'Long Lasting Artisan Perfume With Notes Of Papaya, Peach, And Coconut scent',
-      price: '\$25',
-    ),
-    ProductItem(
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/11/29/13/39/analog-watch-1869928_1280.jpg',
-      title: 'Watch',
-      description: ' A classic, easy-to-read timepiece for everyday wear',
-      price: '\$57',
-    ),
-    ProductItem(
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2017/10/16/14/28/style-2857387_960_720.jpg',
-      title: 'Sandals',
-      description:
-          ' Block high heel sandals are easy and comfortable to walk in, ',
-      price: '\$40',
-    ),
-    ProductItem(
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2015/08/05/09/55/mens-shoes-875950_1280.jpg',
-      title: 'Shoes',
-      description: ' These loafer flats take you from day to night ',
-      price: '\$35',
-    ),
+class _HomeScreenState extends State<HomeScreen> {
+  List<Item> products = [
+    Item(
+        name: 'Heels',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2016/07/25/13/52/woman-1540543_640.jpg',
+        price: 55.0),
+    Item(
+        name: 'Handbags',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2017/10/11/21/03/red-2842438_1280.jpg',
+        price: 95.0),
+    Item(
+        name: 'Wrist Watch',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2017/03/20/15/13/wrist-watch-2159351_640.jpg',
+        price: 45.0),
+    Item(
+        name: 'Perfume',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2017/03/14/11/36/perfume-2142792_960_720.jpg',
+        price: 50.0),
+    Item(
+        name: 'Jordans',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2021/02/03/21/17/sneakers-5979353_640.jpg',
+        price: 35.0),
+    Item(
+        name: 'Jacket',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2017/09/07/04/54/khaki-2723896_640.jpg',
+        price: 65.0),
+    Item(
+        name: 'Nike',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2019/01/26/22/48/nike-3957127_1280.jpg',
+        price: 45.0),
+    Item(
+        name: 'Bracelet',
+        imageUrl:
+            'https://cdn.pixabay.com/photo/2015/03/09/10/03/jewelry-665330_640.jpg',
+        price: 25.0),
   ];
+
+  List<Item> items = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        backgroundColor: Colors.blueGrey,
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 8.0,
+          childAspectRatio: 0.68,
         ),
-        padding: const EdgeInsets.all(8.0),
-        itemCount: _buildProductItems.length,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.network(
-                  _buildProductItems[index].imageUrl,
-                  height: 100,
-                  width: 100,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  _buildProductItems[index].title,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Card(
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Image.network(
+                      products[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  _buildProductItems[index].description,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.0,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      products[index].name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  _buildProductItems[index].price,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('\$${products[index].price}'),
                   ),
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          addToCart(products[index]);
+                        },
+                        icon: const Icon(Icons.add_shopping_cart),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         },
       ),
     );
   }
+
+  void addToCart(Item product) {
+    setState(() {
+      items.add(product);
+    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CartView(
+          items: items,
+          onDelete: (index) {
+            setState(() {
+              items.removeAt(index);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Item {
+  final String name;
+  final String imageUrl;
+  final double price;
+
+  Item({required this.name, required this.imageUrl, required this.price});
 }
